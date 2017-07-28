04effe281dc43b6bdaa32edf5e2c29f685b9dc22
Unknown <ziyang0621@gmail.com>
2017-07-28 11:56:50 -0700

    We did this in GitKraken

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+    
+        print(buttonCount)
+        
+        if buttonCount >= 10 {
+            
+            view.backgroundColor = UIColor.red
+    
+            myLabel.text = "You hit it more than 10 times!"
+        }
+        
+        if buttonCount >= 15 {
+            
+            view.backgroundColor = UIColor.green
+            
+            myLabel.text = "You hit it more than 15 times!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
